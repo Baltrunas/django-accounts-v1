4d7717f5621370af93d663805337b770f41e3c55
Stanislav Baltrunas <stanislav@baltrunas.ru>
2012-10-25 14:43:32 +0600

    PEP-8 fix

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
-# add to urls.py
-	# Users profile
-	url(r'^profile/', include('profile.urls')),
+# Django-Catalog
+A simple catalog.
 
-# add to INSTALLED_APPS in settinhs.py
-	'profile',
+# About
+A simple catalog.
+
+# Install
+* add to urls.py url(r'^profile/', include('profile.urls')),
+* add to INSTALLED_APPS in settinhs.py 'profile',
+
+# Fow to use
+Just use...
+
+# Futures
+
+# ChangeLog
+## 2012.10.25
+* PEP-8
+* Update README.md
--- urls.py
+++ urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, url
-# from profile.views import signup, login, logout, my_profile
+from django.conf.urls import patterns
+from django.conf.urls import url
 
 
 urlpatterns = patterns('',
 	url(r'^signup/$', 'profile.views.signup', name='signup'),
 	url(r'^profile/$', 'profile.views.my_profile', name='profile'),
-	url(r'^login/$', 'django.contrib.auth.views.login', { "template_name": "profile/login.html"}, name='login'),
+	url(r'^login/$', 'django.contrib.auth.views.login', {"template_name": "profile/login.html"}, name='login'),
 	url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
 )
